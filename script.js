6164644f337543334ce5978860ea4be46740ccb6
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-04-03 21:12:17 -0300

    código js para aparecer a capa de 3 filmes

---
+++ script.js
@@ -0,0 +1,9 @@
+let listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+
+for(var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src =" + listaFilmes[i] + ">")
+
+}
+
+
