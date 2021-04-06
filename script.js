8533351d8aaf5fac9544bfc59f0821e4d5c9ea45
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-04-05 22:05:34 -0300

    corrigido o erro das imagens, adicionando a div #images e utilizando innerHTML

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+let listaFilmes = [
+  "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+];
 
-
-for(var i = 0; i < listaFilmes.length; i++) {
-    document.write("<img src =" + listaFilmes[i] + ">")
-
+for (var i = 0; i < listaFilmes.length; i++) {
+    var filmes = document.querySelector("#images")
+    var imagensFilmes = "<img src =" + listaFilmes[i] + ">"
+    filmes.innerHTML = filmes.innerHTML + imagensFilmes
 }
 
 
